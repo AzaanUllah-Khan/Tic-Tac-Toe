6ad933a868ac0471eafc5db5f40648f6287a25e3
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2024-02-12 06:58:52 +0500

    animation added on winning and draw swal removed

--- script.js
+++ script.js
@@ -27,136 +27,166 @@ function game(btn) {
 }
 
 function updateVariables() {
-    one = document.getElementById("1").innerHTML;
-    two = document.getElementById("2").innerHTML;
-    three = document.getElementById("3").innerHTML;
-    four = document.getElementById("4").innerHTML;
-    five = document.getElementById("5").innerHTML;
-    six = document.getElementById("6").innerHTML;
-    seven = document.getElementById("7").innerHTML;
-    eight = document.getElementById("8").innerHTML;
-    nine = document.getElementById("9").innerHTML;
+    one = document.getElementById("1")
+    two = document.getElementById("2")
+    three = document.getElementById("3")
+    four = document.getElementById("4")
+    five = document.getElementById("5")
+    six = document.getElementById("6")
+    seven = document.getElementById("7")
+    eight = document.getElementById("8")
+    nine = document.getElementById("9")
 }
 
 function check() {
-    if ((one == "X" && two == "X" && three == "X") || (one == "O" && two == "O" && three == "O")) {
-        Swal.fire({
-            title: turn + " Won",
-            icon: "success",
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#101032",
-            confirmButtonText: "Play Again"
-        }).then((result) => {
-            if (result.isConfirmed) {
-                location.reload()
-            }
-        });
-    } else if ((four == "X" && five == "X" && six == "X") || (four == "O" && five == "O" && six == "O")) {
-        Swal.fire({
-            title: turn + " Won",
-            icon: "success",
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#101032",
-            confirmButtonText: "Play Again"
-        }).then((result) => {
-            if (result.isConfirmed) {
-                location.reload()
-            }
-        });
-    } else if ((seven == "X" && eight == "X" && nine == "X") || (seven == "O" && eight == "O" && nine == "O")) {
-        Swal.fire({
-            title: turn + " Won",
-            icon: "success",
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#101032",
-            confirmButtonText: "Play Again"
-        }).then((result) => {
-            if (result.isConfirmed) {
-                location.reload()
-            }
-        });
-    } else if ((one == "X" && four == "X" && seven == "X") || (one == "O" && four == "O" && seven == "O")) {
-        Swal.fire({
-            title: turn + " Won",
-            icon: "success",
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#101032",
-            confirmButtonText: "Play Again"
-        }).then((result) => {
-            if (result.isConfirmed) {
-                location.reload()
-            }
-        });
-    } else if ((two == "X" && five == "X" && eight == "X") || (two == "O" && five == "O" && eight == "O")) {
-        Swal.fire({
-            title: turn + " Won",
-            icon: "success",
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#101032",
-            confirmButtonText: "Play Again"
-        }).then((result) => {
-            if (result.isConfirmed) {
-                location.reload()
-            }
-        });
-    } else if ((three == "X" && six == "X" && nine == "X") || (three == "O" && six == "O" && nine == "O")) {
-        Swal.fire({
-            title: turn + " Won",
-            icon: "success",
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#101032",
-            confirmButtonText: "Play Again"
-        }).then((result) => {
-            if (result.isConfirmed) {
-                location.reload()
-            }
-        });
-    } else if ((one == "X" && five == "X" && nine == "X") || (one == "O" && five == "O" && nine == "O")) {
-        Swal.fire({
-            title: turn + " Won",
-            icon: "success",
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#101032",
-            confirmButtonText: "Play Again"
-        }).then((result) => {
-            if (result.isConfirmed) {
-                location.reload()
-            }
-        });
-    } else if ((three == "X" && five == "X" && seven == "X") || (three == "O" && five == "O" && seven == "O")) {
-        Swal.fire({
-            title: turn + " Won",
-            icon: "success",
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#101032",
-            confirmButtonText: "Play Again"
-        }).then((result) => {
-            if (result.isConfirmed) {
-                location.reload()
-            }
-        });
+    if ((one.innerHTML == "X" && two.innerHTML == "X" && three.innerHTML == "X") || (one.innerHTML == "O" && two.innerHTML == "O" && three.innerHTML == "O")) {
+        one.style.backgroundColor = "#101032b7"
+        one.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        two.style.backgroundColor = "#101032b7"
+        two.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        three.style.backgroundColor = "#101032b7"
+        three.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        document.getElementById("winner").innerHTML = turn + " Won"
+        document.getElementById("winner").style.display = "block"
+        var btn = document.getElementsByTagName("button")
+        for (i = 0; i < btn.length; i++) {
+            btn[i].disabled = true
+        }
+        document.getElementById("reset").disabled = false
+    } else if ((four.innerHTML == "X" && five.innerHTML == "X" && six.innerHTML == "X") || (four.innerHTML == "O" && five.innerHTML == "O" && six.innerHTML == "O")) {
+        four.style.backgroundColor = "#101032b7"
+        four.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        five.style.backgroundColor = "#101032b7"
+        five.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        six.style.backgroundColor = "#101032b7"
+        six.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        document.getElementById("winner").innerHTML = turn + " Won"
+        document.getElementById("winner").style.display = "block"
+        var btn = document.getElementsByTagName("button")
+        for (i = 0; i < btn.length; i++) {
+            btn[i].disabled = true
+        }
+        document.getElementById("reset").disabled = false
+    } else if ((seven.innerHTML == "X" && eight.innerHTML == "X" && nine.innerHTML == "X") || (seven.innerHTML == "O" && eight.innerHTML == "O" && nine.innerHTML == "O")) {
+        seven.style.backgroundColor = "#101032b7"
+        seven.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        eight.style.backgroundColor = "#101032b7"
+        eight.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        nine.style.backgroundColor = "#101032b7"
+        nine.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        document.getElementById("winner").innerHTML = turn + " Won"
+        document.getElementById("winner").style.display = "block"
+        var btn = document.getElementsByTagName("button")
+        for (i = 0; i < btn.length; i++) {
+            btn[i].disabled = true
+        }
+        document.getElementById("reset").disabled = false
+    } else if ((one.innerHTML == "X" && four.innerHTML == "X" && seven.innerHTML == "X") || (one.innerHTML == "O" && four.innerHTML == "O" && seven.innerHTML == "O")) {
+        one.style.backgroundColor = "#101032b7"
+        one.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        four.style.backgroundColor = "#101032b7"
+        four.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        seven.style.backgroundColor = "#101032b7"
+        seven.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        document.getElementById("winner").innerHTML = turn + " Won"
+        document.getElementById("winner").style.display = "block"
+        var btn = document.getElementsByTagName("button")
+        for (i = 0; i < btn.length; i++) {
+            btn[i].disabled = true
+        }
+        document.getElementById("reset").disabled = false
+    } else if ((two.innerHTML == "X" && five.innerHTML == "X" && eight.innerHTML == "X") || (two.innerHTML == "O" && five.innerHTML == "O" && eight.innerHTML == "O")) {
+        two.style.backgroundColor = "#101032b7"
+        two.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        five.style.backgroundColor = "#101032b7"
+        five.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        eight.style.backgroundColor = "#101032b7"
+        eight.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        document.getElementById("winner").innerHTML = turn + " Won"
+        document.getElementById("winner").style.display = "block"
+        var btn = document.getElementsByTagName("button")
+        for (i = 0; i < btn.length; i++) {
+            btn[i].disabled = true
+        }
+        document.getElementById("reset").disabled = false
+    } else if ((three.innerHTML == "X" && six.innerHTML == "X" && nine.innerHTML == "X") || (three.innerHTML == "O" && six.innerHTML == "O" && nine.innerHTML == "O")) {
+        three.style.backgroundColor = "#101032b7"
+        three.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        six.style.backgroundColor = "#101032b7"
+        six.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        nine.style.backgroundColor = "#101032b7"
+        nine.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        document.getElementById("winner").innerHTML = turn + " Won"
+        document.getElementById("winner").style.display = "block"
+        var btn = document.getElementsByTagName("button")
+        for (i = 0; i < btn.length; i++) {
+            btn[i].disabled = true
+        }
+        document.getElementById("reset").disabled = false
+    } else if ((one.innerHTML == "X" && five.innerHTML == "X" && nine.innerHTML == "X") || (one.innerHTML == "O" && five.innerHTML == "O" && nine.innerHTML == "O")) {
+        one.style.backgroundColor = "#101032b7"
+        one.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        five.style.backgroundColor = "#101032b7"
+        five.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        nine.style.backgroundColor = "#101032b7"
+        nine.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        document.getElementById("winner").innerHTML = turn + " Won"
+        document.getElementById("winner").style.display = "block"
+        var btn = document.getElementsByTagName("button")
+        for (i = 0; i < btn.length; i++) {
+            btn[i].disabled = true
+        }
+        document.getElementById("reset").disabled = false
+    } else if ((three.innerHTML == "X" && five.innerHTML == "X" && seven.innerHTML == "X") || (three == "O" && five.innerHTML == "O" && seven.innerHTML == "O")) {
+        three.style.backgroundColor = "#101032b7"
+        three.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        five.style.backgroundColor = "#101032b7"
+        five.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        seven.style.backgroundColor = "#101032b7"
+        seven.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        document.getElementById("winner").innerHTML = turn + " Won"
+        document.getElementById("winner").style.display = "block"
+        var btn = document.getElementsByTagName("button")
+        for (i = 0; i < btn.length; i++) {
+            btn[i].disabled = true
+        }
+        document.getElementById("reset").disabled = false
     } else if (
-        (one == "X" || one == "O") &&
-        (two == "X" || two == "O") &&
-        (three == "X" || three == "O") &&
-        (four == "X" || four == "O") &&
-        (five == "X" || five == "O") &&
-        (six == "X" || six == "O") &&
-        (seven == "X" || seven == "O") &&
-        (eight == "X" || eight == "O") &&
-        (nine == "X" || nine == "O")
+        (one.innerHTML == "X" || one.innerHTML == "O") &&
+        (two.innerHTML == "X" || two.innerHTML == "O") &&
+        (three.innerHTML == "X" || three.innerHTML == "O") &&
+        (four.innerHTML == "X" || four.innerHTML == "O") &&
+        (five.innerHTML == "X" || five.innerHTML == "O") &&
+        (six.innerHTML == "X" || six.innerHTML == "O") &&
+        (seven.innerHTML == "X" || seven.innerHTML == "O") &&
+        (eight.innerHTML == "X" || eight.innerHTML == "O") &&
+        (nine.innerHTML == "X" || nine.innerHTML == "O")
     ) {
-        Swal.fire({
-            title: "It's a Draw",
-            icon: "info",
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#101032",
-            confirmButtonText: "Play Again"
-        }).then((result) => {
-            if (result.isConfirmed) {
-                location.reload();
-            }
-        });
+        one.style.backgroundColor = "#101032b7"
+        one.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        two.style.backgroundColor = "#101032b7"
+        two.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        three.style.backgroundColor = "#101032b7"
+        three.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        four.style.backgroundColor = "#101032b7"
+        four.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        five.style.backgroundColor = "#101032b7"
+        five.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        six.style.backgroundColor = "#101032b7"
+        six.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        seven.style.backgroundColor = "#101032b7"
+        seven.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        eight.style.backgroundColor = "#101032b7"
+        eight.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        nine.style.backgroundColor = "#101032b7"
+        nine.style.boxShadow = "1px 1px 10px 0 rgba(0,0,0,0.3)"
+        document.getElementById("winner").innerHTML = turn + " Won"
+        document.getElementById("winner").style.display = "block"
+        var btn = document.getElementsByTagName("button")
+        for (i = 0; i < btn.length; i++) {
+            btn[i].disabled = true
+        }
+        document.getElementById("reset").disabled = false
     }
 }
 function changeBtnCol() {
